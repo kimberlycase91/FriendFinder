f7b0b80d24610360c9a9fc3260e4d84842390f5c
kimberlycase91 <kimberlycase91@gmail.com>
2020-03-05 12:59:31 -0700

    fixed file paths, server can now connect and display correct pages

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,13 @@
-var surveyData = require("..data/sur");
+var allFriends = require("../data/friends.js");
 
 module.exports = function(app) {
-    app.get("/api/results", function(req, res){
-        res.json(surveyData);
+    app.get("/api/friends", function(req, res){
+        res.json(allFriends);
+    })
+
+    app.post("/api/friends", function(req,res){
+        var newFriend = req.body;
+        allFriends.push(newFriend);
+        res.json(newFriend);
     })
 }
